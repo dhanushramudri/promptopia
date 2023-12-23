e3a7ee6ebbc94b4d99a6cdf9fb2faa286a8f7ec3
dhanushramudri <ramudridhanush@gmail.com>
2023-12-23 23:59:18 +0530

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-orange": "#FF5722",
+      },
+    },
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  plugins: [],
+};
